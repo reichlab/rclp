043de15e1f622a1c73db894297703a25313beddc
Evan Ray <elray@umass.edu>
2022-09-26 23:51:03 -0400

    define as module instead of package

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='rclp',
-    version='0.0.1',
+setup(
+    name='rclp',
+    version='1.0.0',
     description='Re-calibrated linear pools',
     author='Serena Wang, Evan L. Ray',
     author_email='elray@umass.edu',
     url='https://github.com/reichlab/rclp',
-    packages=['rclp'],
+    py_modules=['rclp'],
+    install_requires=[
+        'numpy',
+        'tensorflow>=2',
+        'tensorflow_probability>=0.16.0'
+    ]
 )
--- test/test_handle_missingness.py
+++ test/test_handle_missingness.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 import unittest
 
-from rclp.rclp import BetaMixtureRCLP
+from rclp import BetaMixtureRCLP
 
 
 class Test_HandleMissingness(unittest.TestCase):
